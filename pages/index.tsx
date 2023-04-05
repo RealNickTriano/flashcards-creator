704b84cbfc7530138bb269c7a55432065d739b61
Nicholas Triano <nicholastriano1999@gmail.com>
2023-04-05 13:04:35 -0400

    github Oauth

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import LoginBtn from '@/components/LoginBtn'
+import { useSession } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! From dev!
-    </h1>
+    <>
+    {
+      session ? 
+      <>
+        <h1 className="text-3xl font-bold underline">
+          Welcome, {session?.user?.name}
+        </h1>
+      </>
+      :
+        <h1 className="text-3xl font-bold underline">
+          Please Sign In Below
+        </h1>
+    }
+      <LoginBtn></LoginBtn>
+    </>
+    
   )
 }
