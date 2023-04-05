2c75ac07d8067e7082a3523904bf02e80bf41372
Nicholas Triano <nicholastriano1999@gmail.com>
2023-04-05 16:48:53 -0400

    Flashcard present layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import LoginBtn from '@/components/LoginBtn'
 import { useSession } from 'next-auth/react'
+import Flashcard from '@/components/Flashcard'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +13,8 @@ export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <>
-    {
+    <div className='flex flex-col justify-center gap-12 items-center h-screen'>
+    {/* {
       session ? 
       <>
         <h1 className="text-3xl font-bold underline">
@@ -23,9 +25,16 @@ export default function Home() {
         <h1 className="text-3xl font-bold underline">
           Please Sign In Below
         </h1>
-    }
-      <LoginBtn></LoginBtn>
-    </>
+    } */}
+      {/* <LoginBtn></LoginBtn> */}
+      <NavBar></NavBar>
+      <Flashcard
+        question={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar tortor massa, varius auctor ligula sollicitudin sit amet. Donec mollis sollicitudin velit, a rhoncus augue gravida non."}
+        answer={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar tortor massa, varius auctor ligula sollicitudin sit amet. Donec "}
+        flip={false}
+        edit={false}
+      ></Flashcard>
+    </div>
     
   )
 }
