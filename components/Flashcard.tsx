60854176d604119481db50cf6a3d3f2d49eb1e2d
Nicholas Triano <nicholastriano1999@gmail.com>
2023-04-05 17:07:06 -0400

    fixed hydration error

--- components/Flashcard.tsx
+++ components/Flashcard.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react"
+import { FunctionComponent, useEffect, useState } from "react"
 import { MdEditNote } from "react-icons/md";
 
-const Flashcard = ({ question, answer, flip, edit }) => {
+type Props = {
+    question: string,
+    answer: string,
+    flip: boolean,
+    edit: boolean,
+}
+
+const Flashcard: FunctionComponent<Props> = ({ question, answer, flip, edit }) => {
     const [flipped, setFlipped] = useState(flip ? flip : false);
     const [editable, setEditable] = useState(edit ? edit : false);
 
     // Need this to stop parent button from also activating
-    const handleEdit = (e) => {
+    const handleEdit = (e: React.MouseEvent<HTMLElement>) => {
         e.stopPropagation();
         e.preventDefault();
         setEditable(!editable);
@@ -15,22 +22,24 @@ const Flashcard = ({ question, answer, flip, edit }) => {
     }
 
   return (
-    <button 
+    <div 
         className="h-[500px] w-[900px] shadow-md bg-white flex flex-col justify-start items-center px-16 py-8 gap-6 rounded-md"
         onClick={() => setFlipped(!flipped)}
     >
         <div className="grid grid-cols-3 justify-center items-center w-full">
-            <h1 className="font-medium text-4xl col-start-2">{!flipped ? "Question" : "Answer"}</h1>
+            <h1 className="font-medium text-4xl col-start-2">       
+                {!flipped ? "Question" : "Answer"}
+            </h1>
             <button 
-                    className="flex justify-center items-center gap-2 text-blue-600 place-self-end"
-                    onClick={ handleEdit }
-                ><MdEditNote size={30}/>
+                className="flex justify-center items-center gap-2 text-blue-600 place-self-end"
+                onClick={ handleEdit }
+            ><MdEditNote size={30}/>
             </button>
         </div>
         <div className="font-normal text-4xl">
             { !flipped ? question : answer }
         </div>
-    </button>
+    </div>
   )
 }
 
